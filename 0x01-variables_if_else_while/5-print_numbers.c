695938fde07794d78621e591e08e0bc675520127
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-06-19 16:59:49 -0500

    print numbers base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int number = 0;
+for (int i = 0; number <= 9; i++)
+{
+printf("%d", number);
+number++;
+}
+printf("\n");
+}
