2ba7750cea485cb2e2f40eb166354f3c7566d79e
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-06-19 13:54:21 -0500

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ printf("Last digit of %d is %d and is 0\n", n, a);
 }
 else
 {
-printf("Last digit of %d is %d and is lees than 6 and not 0\n", n, a);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 }
 return (0);
 }
