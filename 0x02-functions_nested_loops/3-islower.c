3d1b326f85e2a4c338249e3c777aba44899dc5c2
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-06-22 14:45:52 -0500

    function _islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 /**
- * _islower - search lowercase.
- *
- * Return: Always value.
+ * _islower - tests whether a character is a
+ * lowercase letter from the English alphabet.
+ * @c: character to test.
+ * Return: 1 if it is a lowercase letter, 0 if it is not a lowercase letter.
  */
 int _islower(int c)
 {
