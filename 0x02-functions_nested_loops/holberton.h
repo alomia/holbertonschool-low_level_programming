d12374423f96ed9e15655325b8ab9033287731cd
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-06-22 09:46:08 -0500

    add new funcion

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -7,7 +7,10 @@
 /*screen printing function*/
 int _putchar(char c);
 
-/*function to print the albeto*/
+/*function to print the alphabet*/
 void print_alphabet(void);
+
+/*function to print alphabet x10*/
+void print_alphabet_x10(void);
 
 #endif
