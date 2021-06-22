e158bea51eccec111734f0062eee8907cb016ee3
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-06-22 09:37:59 -0500

    function print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,15 @@
 #include "holberton.h"
+/**
+ * print_alphabet - print alphabet.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char i;
 
-void print_alphabet(void) {
-    char i;
+for (i = 'a' ; i <= 'z'; i++)
+_putchar(i);
 
-    for ( i = 'a' ; i <= 'z'; i++) {
-        _putchar(i);
-    }
-    _putchar('\n');
+_putchar('\n');
 }
