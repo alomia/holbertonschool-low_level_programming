531a30a98634419f497209ced3e587f951ce3c23
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-06-23 18:40:56 -0500

    return absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _abs - return the value absolute
+ *
+ * @n: integer to be evaluated.
+ * Return: n = n * -1.
+ */
+int _abs(int n)
+{
+
+if (n < 0)
+return (n = n * -1);
+
+return (0);
+}
