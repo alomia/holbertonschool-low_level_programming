12a4a7739c1f4c9e9b1b11f52380e8fc448415ef
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-06-23 23:40:57 -0500

    function adds integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - adds two integers
+ *
+ * @a: integer to be evaluated.
+ * @b: integer to be evaluated.
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
