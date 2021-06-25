0c55b6b91840b2a201526d8bac98bb01974615f9
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-06-24 23:02:13 -0500

    function return 1 if is uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,17 @@
 #include "holberton.h"
 
 /**
-* _isdigit - function return 1 if c is digit
+* _isupper - function return 1 if c is uppercase
 * @c: variable
-* Return: 1 if is digit or 0 if no is a digit
+* Return: 1 if is uppercase or 0 if is lowercase
 */
 
-int _isdigit(int c)
+int _isupper(int c)
 {
 
-if (c >= 48 && c <= 57)
+if (c >= 65 && c <= 90)
 return (1);
 
 else
 return (0);
-
 }
