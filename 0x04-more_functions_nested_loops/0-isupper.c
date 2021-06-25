7ef72407fcbc82e60e6ce5f7b96c292a82166f3b
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-06-24 22:36:33 -0500

    add function uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+int _isupper(int c)
+{
+
+if (c <= 90)
+return (1);
+
+else
+return (0);
+
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains some functions for improvement and learning purposes.
