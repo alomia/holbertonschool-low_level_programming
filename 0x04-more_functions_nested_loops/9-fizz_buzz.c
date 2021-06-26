29f9fda9f248e7ce9167a44e0faf3ce94f6d08df
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-06-25 20:23:09 -0500

    add function print numbers mult of 3 and 5

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,21 +14,24 @@ int i;
 
 for (i = 1 ; i <= 100 ; i++)
 {
-if ((i % 3) == 0 && (i % 5) == 0)
-{
-printf("FizzBuzz ");
-}
-else if ((i % 3) == 0)
-{
-printf("Fizz ");
-}
-else if ((i % 5) == 0)
-{
-printf("Buzz ");
-}
-printf("%d ", i);
-}
-printf("\n");
+    
+if ((i % 15) == 0)
+printf("FizzBuzz");
 
+else if ((i % 3) == 0)
+printf("Fizz");
+
+else if ((i % 5) == 0)
+
+printf("Buzz");
+
+printf("%d", i);
+
+if (i < 100)
+printf(" ");
+
+}
+
+printf("\n");
 return (0);
 }
