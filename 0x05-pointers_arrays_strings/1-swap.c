a5f4b9614097eaad48fa0c62a5117f44365338fc
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-06-28 21:43:24 -0500

    function that swaps the values of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer
+ * @b: pointer
+ *
+ * return: void
+ */
+void swap_int(int *a, int *b)
+{
+
+int aux;
+
+aux = *a;
+*a = *b;
+*b = aux;
+
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -7,4 +7,7 @@ int _putchar(char c);
 /*function that takes a pointer to an int 98*/
 void reset_to_98(int *n);
 
+/*function that swaps the values of two integers*/
+void swap_int(int *a, int *b);
+
 #endif
