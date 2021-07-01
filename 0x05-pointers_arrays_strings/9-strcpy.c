021595c401a05eab57fc8ae54550631e20fb90fb
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-07-01 00:14:00 -0500

    function that copies the string pointed

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * _strcpy -  copies the string pointed to by src
+ * @dest: pointer
+ * @src: pointer
+ *
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+int i = 0;
+
+while (*(src + i))
+
+{
+
+*(dest + i) = *(src + i);
+
+i++;
+
+}
+
+*(dest + i) = '\0';
+
+return (dest);
+
+}
