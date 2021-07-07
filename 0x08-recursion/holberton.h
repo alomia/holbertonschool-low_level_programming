14298c8e8c29292287233237b9432c71b6fdf24c
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-07-07 18:24:53 -0500

    add new directory and new function prints a string, followed by a new line.

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/*writes the character c to stdout*/
+int _putchar(char c);
+
+/*function that prints a string, followed by a new line*/
+void _puts_recursion(char *s);
+
+#endif
