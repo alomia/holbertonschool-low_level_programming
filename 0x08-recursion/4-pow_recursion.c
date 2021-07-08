acd0df445a64b1de61c744c397a1746f67a549e4
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-07-07 21:41:38 -0500

    function that returns the value of x raised to the power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - function that returns the value of x
+ * raised to the power of y
+ * @x: variable
+ * @y: variable
+ *
+ * Return: result or 1 or -1
+ */
+int _pow_recursion(int x, int y)
+{
+
+int result;
+
+if (y > 0)
+{
+
+result = (x * (_pow_recursion(x, y)));
+return (result);
+}
+
+if (y == 0)
+return (1);
+
+else
+return (-1);
+
+}
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -15,5 +15,7 @@ int _strlen_recursion(char *s);
 
 /*function that returns the factorial of a given number*/
 int factorial(int n);
+/*function that returns the value of x raised to the power of y*/
+int _pow_recursion(int x, int y);
 
 #endif
