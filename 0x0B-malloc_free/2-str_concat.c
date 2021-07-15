01ee67fd9b3a6d172522c329e554fd91701669ae
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-07-14 21:45:24 -0500

    update functions, 1 and 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,30 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * _strdup - function that returns a pointer to a
- * newly allocated space in memory
- * @str: pointer
+ * str_concat - function that concatenates two strings
+ * @s1: pointer
+ * @s2: pointer
  *
- * Return: ptrArray
+ * Return: ptr_str
  */
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
 
 char *ptr_str;
-
 int len;
 
 len = 0;
 
-
-len =  strlen(str);
+len = strlen(s1) + strlen(s2);
 
 ptr_str = malloc((len + 1) * sizeof(char));
 
 if (ptr_str == NULL)
 return (NULL);
 
-strcpy(ptr_str, str);
+strcat(ptr_str, s1);
+strcat(ptr_str, s2);
 
 return (ptr_str);
 }
