cbf3f2db4f7ab6ff15ed7fd7ec9103fe11b890af
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-07-19 23:23:02 -0500

    function that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - function that frees dogs
+ * @d: pointer
+ *
+ * REturn: void
+ */
+void free_dog(dog_t *d)
+{
+
+free(d->name);
+free(d->owner);
+}
