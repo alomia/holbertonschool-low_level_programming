95bdf36f722ee4c33982645c9392127cc50fedc1
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-07-19 22:39:15 -0500

    update file dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - struct
+ * @name: pointer
+ * @age: variable
+ * @owner: pointer
+ *
+ */
+struct dog
+{
 
-typedef struct dog {
-
-        char *name;
-        float age;
-        char *owner;
-} dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
-void print_dog(struct dog *d);
+char *name;
+float age;
+char *owner;
+};
 
 #endif
