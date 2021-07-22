81f716a4caacf48f15ba899e1d30d3ccc7199ca8
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-07-22 18:58:33 -0500

    programs 3

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,38 @@
+#ifndef CALC_H
+#define CALC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+
+char *op;
+int (*f)(int a, int b);
+} op_t;
+
+/*returns the sum of a and b*/
+int op_add(int a, int b);
+
+/*returns the difference of a and b*/
+int op_sub(int a, int b);
+
+/*returns the product of a and b*/
+int op_mul(int a, int b);
+
+/*returns the result of the division of a by b*/
+int op_div(int a, int b);
+
+/*returns the remainder of the division of a by b*/
+int op_mod(int a, int b);
+
+/*function to perform the operation asked by the user*/
+int (*get_op_func(char *s))(int, int);
+
+#endif
