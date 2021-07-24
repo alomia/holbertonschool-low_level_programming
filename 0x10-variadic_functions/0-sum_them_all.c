c392ade749bf91ec0a586e2155bb5a9723c9c330
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-07-23 19:21:10 -0500

    update, firs commit sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,20 +8,17 @@
 int sum_them_all(const unsigned int n, ...)
 {
 
+unsigned int i, sum;
+va_list ap;
+
 if (n == 0)
 return (0);
 
-int i, sum;
-
-va_list ap;
 va_start(ap, n);
-
 sum = 0;
 
 for (i = 0; i < n ; i++)
-{
 sum += va_arg(ap, int);
-}
 
 return (sum);
 }
