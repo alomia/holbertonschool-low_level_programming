e31086b0ffe4a27a0be371baca686cc0e2e7a0ce
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-07-23 19:52:47 -0500

    add function sum using stdarg

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,19 +8,19 @@
 int sum_them_all(const unsigned int n, ...)
 {
 
-va_list args;
+va_list ap;
 unsigned int i;
 unsigned int sum = 0;
 
-va_start(args, n);
+va_start(ap, n);
 
 if (n == 0)
 return (0);
 
-for (i = 0; i < n ; i++)
-sum += va_arg(args, unsigned int);
+for (i = 0; i < n; i++)
+sum = sum + va_arg(ap, unsigned int);
 
-va_end(args);
+va_end(ap);
 
 return (sum);
 }
