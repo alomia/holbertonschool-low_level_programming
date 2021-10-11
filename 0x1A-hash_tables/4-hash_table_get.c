9cb2fe3acea8c50525000719aee90df0f54d19e2
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-10-10 23:20:25 -0500

    add file: get the value of a key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - function to get the value of a key
+ * @ht: hash table
+ * @key: key
+ *
+ * Return: NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+
+unsigned long int idx;
+hash_node_t *node;
+
+if (ht == NULL || key == NULL || strlen(key) == 0)
+        return (NULL);
+
+idx = key_index((unsigned char *) key, ht->size);
+
+if (ht->array[idx] == NULL || ht->array[idx] == 0)
+        return (NULL);
+
+if (strcmp(ht->array[idx]->key, key) == 0)
+        return (ht->array[idx]->value);
+
+node = ht->array[idx];
+
+while (node != NULL)
+{
+
+if (strcmp(node->key, key) == 0)
+        return (node->value);
+node = node->next;
+}
+
+return (NULL);
+}
