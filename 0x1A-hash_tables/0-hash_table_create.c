475b7623f10ad6dd426d1c2a84ed59a428fb1fb7
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-10-10 22:49:27 -0500

    add file: create table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,11 +10,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 hash_table_t *ht;
 
+if (size < 1)
+return (NULL);
+
 ht = malloc(sizeof(hash_table_t));
+
 if (ht == NULL)
 return (NULL);
 
-ht->array = malloc(size * (sizeof(hash_node_t *)));
+ht->size = size;
+ht->array = calloc(size, sizeof(hash_node_t *));
 
 if (ht->array == NULL)
 {
@@ -22,8 +27,6 @@ if (ht->array == NULL)
 free(ht);
 return (NULL);
 }
-
-ht->size = size;
 
 return (ht);
 }
