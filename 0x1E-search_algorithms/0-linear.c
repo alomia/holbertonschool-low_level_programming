2d41928d1a4cdfc99ba0046385724019c16a93a2
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-01-17 23:31:34 -0500

    Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,19 +11,18 @@
 int linear_search(int *array, size_t size, int value)
 {
 
-size_t i = 0;
-
+size_t i;
 
 if (array == NULL || size == 0)
-        return  (-1);
+return (-1);
 
-for (; i < size; ++i)
+for (i = 0; i < size; i++)
 {
-
-printf("value checked array[%ld] = [%d]\n", i, array[i]);
+printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 
 if (array[i] == value)
 return (i);
+
 }
 
 return (-1);
